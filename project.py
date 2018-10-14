24ac62e429a4f5f847c5bad9b6fc3ae507342b5e
Joshua Peek <joshua.s.peek@gmail.com>
2018-10-14 00:12:29 -0500

    +Sign In, Sign Out capability; Admin Pages Protected

--- project.py
+++ project.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+from flask import Flask, render_template, request, redirect
+from flask import url_for, flash, jsonify
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Team, Player
@@ -6,6 +7,19 @@ from sqlalchemy.pool import StaticPool
 from flask import session as login_session
 import random
 import string
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+import httplib2
+import json
+from flask import make_response
+import requests
+
+CLIENT_ID = json.loads(
+open('client_secrets.json', 'r').read())['web']['client_id']
+CLIENT_SECRET = json.loads(
+open('client_secrets.json', 'r').read())['web']['client_secret']
+redirect_uris = json.loads(
+open('client_secrets.json', 'r').read())['web']['redirect_uris']
 
 app = Flask(__name__)
 
@@ -78,7 +92,121 @@ def adminLogin():
     state = ''.join(random.choice(string.ascii_uppercase + string.digits)
     for x in xrange(32))
     login_session['state'] = state
-    return "The current session state is %s" % login_session['state']
+    return render_template('login.html', STATE=state)
+
+@app.route('/gconnect', methods=['POST'])
+def gconnect():
+    # Validate state token
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    # Obtain authorization code
+    code = request.data
+
+    try:
+        # Upgrade the authorization code into a credentials object
+        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+        oauth_flow.redirect_uri = 'postmessage'
+        credentials = oauth_flow.step2_exchange(code)
+    except FlowExchangeError:
+        response = make_response(
+            json.dumps('Failed to upgrade the authorization code.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Check that the access token is valid.
+    access_token = credentials.access_token
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
+           % access_token)
+    h = httplib2.Http()
+    result = json.loads(h.request(url, 'GET')[1])
+    # If there was an error in the access token info, abort.
+    if result.get('error') is not None:
+        response = make_response(json.dumps(result.get('error')), 500)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is used for the intended user.
+    gplus_id = credentials.id_token['sub']
+    if result['user_id'] != gplus_id:
+        response = make_response(
+            json.dumps("Token's user ID doesn't match given user ID."), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Verify that the access token is valid for this app.
+    if result['issued_to'] != CLIENT_ID:
+        response = make_response(
+            json.dumps("Token's client ID does not match app's."), 401)
+        print "Token's client ID does not match app's."
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    stored_access_token = login_session.get('access_token')
+    stored_gplus_id = login_session.get('gplus_id')
+    if stored_access_token is not None and gplus_id == stored_gplus_id:
+        response = make_response(json.dumps('Current user is already connected.'),
+                                 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    # Store the access token in the session for later use.
+    login_session['access_token'] = credentials.access_token
+    login_session['gplus_id'] = gplus_id
+
+    # Get user info
+    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+    params = {'access_token': credentials.access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
+
+    data = answer.json()
+
+    login_session['username'] = data['email']
+    login_session['picture'] = data['picture']
+    login_session['email'] = data['email']
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    flash("you are now logged in as %s" % login_session['username'])
+    print "done!"
+    return output
+
+#Admin Sign-out: Revokes current user's token, resets login_session
+@app.route('/gdisconnect')
+def gdisconnect():
+    #Only disconnect a connected user
+    access_token=login_session.get('access_token')
+    if access_token is None:
+        response = make_response(
+        json.dumps('Current user not connected.'), 401)
+        response.headers['Content-Type']='application/json'
+        return response
+    #Execute HTTP GET to revoke token
+    url='https://accounts.google.com/o/oauth2/revoke?token=%s' % login_session['access_token']
+    h = httplib2.Http()
+    result = h.request(url,'GET')[0]
+    if result['status'] =='200':
+        #Reset the user's session
+        del login_session['access_token']
+        del login_session['gplus_id']
+        del login_session['username']
+        del login_session['email']
+        del login_session['picture']
+        response=make_response(json.dumps('Successfully disconnected.'), 200)
+        response.headers['Content-Type']='application/json'
+        return response
+    else:
+        #The token was invalid for some reason
+        response=make_response(
+        json.dumps('Failed to revoke token for given user.'), 400)
+        response.headers['Content-Type']='application/json'
+        return response
 
 
 
@@ -95,6 +223,8 @@ def adminPage():
 #New Team page
 @app.route('/new/', methods=['GET', 'POST'])
 def newTeamPage():
+    if 'username' not in login_session:
+        return redirect('/login')
     if request.method == 'POST':
         newTeam = Team(
         city=request.form['city'],
@@ -112,6 +242,8 @@ def newTeamPage():
 #New Player page
 @app.route('/<int:team_id>/new/', methods=['GET', 'POST'])
 def newPlayerPage(team_id):
+    if 'username' not in login_session:
+        return redirect('/login')
     if request.method == 'POST':
         newPlayer = Player(firstName=request.form['firstName'],
         lastName=request.form['lastName'],
@@ -136,6 +268,8 @@ def newPlayerPage(team_id):
 #Edit Team page
 @app.route('/<int:team_id>/edit/', methods=['GET', 'POST'])
 def editTeamPage(team_id):
+    if 'username' not in login_session:
+        return redirect('/login')
     editedTeam = session.query(Team).filter_by(id=team_id).one()
     if request.method == 'POST':
         if request.form['city']:
@@ -153,6 +287,8 @@ def editTeamPage(team_id):
 #Edit Player page
 @app.route('/<int:team_id>/<int:player_id>/edit/', methods=['GET', 'POST'])
 def editPlayerPage(team_id, player_id):
+    if 'username' not in login_session:
+        return redirect('/login')
     editedPlayer = session.query(
     Player).filter_by(team_id=team_id, id=player_id).one()
     if request.method == 'POST':
@@ -179,6 +315,8 @@ def editPlayerPage(team_id, player_id):
 #Delete Team page
 @app.route('/<int:team_id>/delete/', methods=['GET', 'POST'])
 def deleteTeamPage(team_id):
+    if 'username' not in login_session:
+        return redirect('/login')
     teamToDelete = session.query(
     Team).filter_by(id=team_id).one()
     if request.method == 'POST':
@@ -192,6 +330,8 @@ def deleteTeamPage(team_id):
 #Delete Player page
 @app.route('/<int:team_id>/<int:player_id>/delete/', methods=['GET', 'POST'])
 def deletePlayerPage(team_id, player_id):
+    if 'username' not in login_session:
+        return redirect('/login')
     playerToDelete = session.query(
     Player).filter_by(team_id=team_id, id=player_id).one()
     if request.method == 'POST':
@@ -204,6 +344,6 @@ def deletePlayerPage(team_id, player_id):
 
 
 if __name__ == '__main__':
-    app.secret_key = 'super_secret_key'
+    app.secret_key = CLIENT_SECRET
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
